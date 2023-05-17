0aa521b19582b215fcc8cd336fefabac5786c223
Andrey Zuev <stepgt@mail.ru>
2023-05-18 09:07:06 +1000

    Added jsonwebtoken && Added route /auth/login

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello stepGT Blog');
+});
+
+app.post('/auth/login', (req, res) => {
+  const token = jwt.sign({ email: req.body.email, fullName: 'stepGT' }, '__secret');
+  res.json({ success: true, token });
 });
 
 app.listen(4444, (err) => {
