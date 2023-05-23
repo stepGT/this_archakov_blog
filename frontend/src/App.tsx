1dff1f2f7e07750c5184623b0a8e0954bc2b75cd
Andrey Zuev <stepgt@mail.ru>
2023-05-24 09:24:22 +1000

    Added page Home && component Header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
-import './App.css';
+import Container from '@mui/material/Container';
+import { Routes, Route } from 'react-router-dom';
+import { Header } from './components';
+import { Home } from './pages';
 
 const App = () => {
-  return <div>stepGT Blog</div>;
+  return (
+    <>
+      <Header />
+      <Container maxWidth="lg">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Container>
+    </>
+  );
 };
 
 export default App;
