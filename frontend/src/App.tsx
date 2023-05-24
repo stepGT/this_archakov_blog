5c8dee7c19bc7c929a296d4e83f0caf97884866f
Andrey Zuev <stepgt@mail.ru>
2023-05-25 08:54:47 +1000

    Added Login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import Container from '@mui/material/Container';
 import { Routes, Route } from 'react-router-dom';
 import { Header } from './components';
-import { Home, FullPost, AddPost } from './pages';
+import { Home, FullPost, AddPost, Login } from './pages';
 
 const App = () => {
   return (
@@ -12,6 +12,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/post/:id" element={<FullPost />} />
           <Route path="/add-post" element={<AddPost />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Container>
     </>
--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,3 +1,4 @@
 export { Home } from './Home';
 export { FullPost } from './FullPost';
-export { AddPost } from './AddPost'
+export { AddPost } from './AddPost';
+export { Login } from './Login';
