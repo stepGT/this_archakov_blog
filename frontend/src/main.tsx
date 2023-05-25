e36c14e05f4e6528083ab49b7809d0bfbbaba10f
Andrey Zuev <stepgt@mail.ru>
2023-05-25 11:43:07 +1000

    Fixed store main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.scss';
 
-import store from './redux/store';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
