ecab21047444ea02cd1075ef0505586ee052b204
Andrey Zuev <stepgt@mail.ru>
2023-05-25 11:41:29 +1000

    Refactor store. Typescript

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { postReducer } from '../redux/slices/posts';
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     posts: postReducer,
   },
 });
 
-export default store;
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
