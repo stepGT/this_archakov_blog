0c6363745e08c89254824bb862ca510f6e17c31a
Andrey Zuev <stepgt@mail.ru>
2023-06-08 11:28:53 +1000

    Added handleChangeFile && Deleted headers Content-Type from axios instance

--- frontend/src/pages/AddPost/index.tsx
+++ frontend/src/pages/AddPost/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useMemo } from 'react';
+import { useState, useCallback, useMemo, useRef, ChangeEvent, FC } from 'react';
 import { selectIsAuth } from '../../redux/features/auth/selectors';
 import { useAppSelector } from '../../redux/store';
 import TextField from '@mui/material/TextField';
@@ -9,17 +9,33 @@ import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
 import { Navigate } from 'react-router-dom';
+import axios from '../../services/axios';
 
-export const AddPost = () => {
+export const AddPost: FC = () => {
+  const inputFileRef = useRef<HTMLInputElement>(null);
   const isAuth = useAppSelector(selectIsAuth);
-  const imageUrl = '';
+  const [imageUrl, setImageUrl] = useState('');
   const [text, setText] = useState('');
   const [title, setTitle] = useState('');
   const [tags, setTags] = useState('');
 
-  const handleChangeFile = () => {};
+  const handleChangeFile = async (e: ChangeEvent<HTMLInputElement>): Promise<void> => {
+    if (!e.target.files) return;
+    try {
+      const formData = new FormData();
+      const file = e.target.files[0];
+      formData.append('image', file);
+      const { data } = await axios.post('/upload', formData);
+      setImageUrl(data.url);
+    } catch (err) {
+      console.warn(err);
+      alert('Ошибка при загрузке файла!');
+    }
+  };
 
-  const onClickRemoveImage = () => {};
+  const onClickRemoveImage = () => {
+    setImageUrl('');
+  };
 
   const onChange = useCallback((value: string) => {
     setText(value);
@@ -36,6 +52,7 @@ export const AddPost = () => {
         autosave: {
           enabled: true,
           delay: 1000,
+          uniqueId: 'uniqueID',
         },
       } as EasyMDE.Options),
     [],
@@ -47,10 +64,10 @@ export const AddPost = () => {
 
   return (
     <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
+      <Button onClick={() => inputFileRef.current?.click()} variant="outlined" size="large">
         Загрузить превью
       </Button>
-      <input type="file" onChange={handleChangeFile} hidden />
+      <input ref={inputFileRef} type="file" onChange={handleChangeFile} hidden />
       {imageUrl && (
         <Button variant="contained" color="error" onClick={onClickRemoveImage}>
           Удалить
